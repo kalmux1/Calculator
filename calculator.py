aabb6ffbec3d370f78377878c7d7c01bc762d53f
kalmux1 <kal.mux.cyber@gmail.com>
2024-08-17 14:56:30 +0530

    Calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,16 @@
 
 # Python calculator
 
+from tkinter import *
+
+base = Tk()
+
+base.geometry("315x500")
+
+base.title("Calculator")
+
+# base.wm_iconbitmap("calculator_icon.ico")
+
+
+
+base.mainloop()
